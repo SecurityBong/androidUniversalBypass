2da4b415c45aed2d5eee0f462d270ef76b274cff
SecurityBong <52169190+SecurityBong@users.noreply.github.com>
2025-03-27 21:11:27 +0530

    androiduniversalbypassv1.7.js

--- androiduniversalbypassv1.7.js
+++ androiduniversalbypassv1.7.js
@@ -6,9 +6,13 @@ var config = {
     debugMode: true,
     targetLibs: ["libpairipcore.so", "libssl.so"],
     rootPackages: [
-        "com.noshufou.android.su", "eu.chainfire.supersu",
-        "com.koushikdutta.superuser", "com.kingouser.com",
-        "com.topjohnwu.magisk"
+        "com.noshufou.android.su", "com.noshufou.android.su.elite", "eu.chainfire.supersu",
+        "com.koushikdutta.superuser", "com.thirdparty.superuser", "com.yellowes.su", "com.koushikdutta.rommanager",
+        "com.koushikdutta.rommanager.license", "com.dimonvideo.luckypatcher", "com.chelpus.lackypatch",
+        "com.ramdroid.appquarantine", "com.ramdroid.appquarantinepro", "com.devadvance.rootcloak", "com.devadvance.rootcloakplus",
+        "de.robv.android.xposed.installer", "com.saurik.substrate", "com.zachspong.temprootremovejb", "com.amphoras.hidemyroot",
+        "com.amphoras.hidemyrootadfree", "com.formyhm.hiderootPremium", "com.formyhm.hideroot", "me.phh.superuser",
+        "eu.chainfire.supersu.pro", "com.kingouser.com", "com.topjohnwu.magisk"
     ],
     rootBinaries: ["su", "busybox", "magisk"]
 };
